7e70b18f2b425f13c5997417bd7867741c6f921a
A <ayhamghraowi@gmail.com>
2016-04-21 17:59:45 +0000

    new frontend styles

--- stillgood.js
+++ stillgood.js
@@ -7,28 +7,31 @@ if(Meteor.isClient){
           return Options.find();
       }
   });
-
-
   
-  Template.addOptions.events({
-      'submit form': function(event){
-          event.preventDefault();
-          Options.insert({
-              name: event.target.optionName.value,
-              url: event.target.optionURL.value
-          });
-          event.target.playerName.value = "";
-          event.target.optionURL.value = "";          
+  Template.modal.helpers({
+      'option': function(){
+          return Options.find();
       }
-  });
-  
+  });  
+
   Template.modal.events({
     'click .myBtn': function(){
       $(".modal").css('display', 'block');
     },
     'click .close': function(){
       $(".modal").css('display', 'none');
-    }
+    },
+     'submit form': function(event){
+          event.preventDefault();
+          Options.insert({
+              name: event.target.optionName.value,
+              url: event.target.optionURL.value,
+              guessdate: event.target.optionDate.value,
+              date: new Date()
+          });
+          event.target.playerName.value = "";
+          event.target.optionURL.value = "";          
+      }
   });
 
 }
